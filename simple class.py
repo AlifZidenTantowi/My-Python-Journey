0d0214931c79f6c5777ce3d0268bf351cf0a7c1b
Alif Ziden Tantowi <99473545+AlifZidenTantowi@users.noreply.github.com>
2025-03-20 21:52:02 +0700

    Update simple class.py
    add new class

--- simple class.py
+++ simple class.py
@@ -6,6 +6,18 @@ class car:
   def drive(self):
     print(f"{self.brand} {self.colour} is driving")
 
+class animal:
+  def __init__(self, name, sound):
+    self.name = name
+    self.sound = sound
+
+  def make_sound(self):
+    print(f"{self.name} wil make sound like this {self.sound}")
+
+my_animal = animal("dog", "bark bark bark arrrrgrrrg")
+
+my_animal.make_sound()
+
 my_car = car("Civic", "Black")
 
 my_car.drive()
